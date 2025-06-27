dbb1c76138a3f49f56a4654cb6020392ebbf02c7
Rado <sitcom93@abv.bg>
2025-06-28 01:45:57 +0300

    Removing and adding new controllers and views
    Introduced new controller and views, improving the robustness of the input processing workflow.

---
+++ SportCenterVictory/Controllers/CrossFitController.cs
@@ -0,0 +1,12 @@
+﻿namespace SportCenterVictory.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class CrossFitController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SportCenterVictory/Controllers/PowerliftingController.cs
@@ -0,0 +1,12 @@
+﻿namespace SportCenterVictory.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class PowerliftingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
