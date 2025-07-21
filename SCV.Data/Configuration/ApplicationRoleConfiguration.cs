b6cb0fafb2c9fb46a9561aea42a55c0f3735d605
Rado <sitcom93@abv.bg>
2025-07-21 22:15:49 +0300

    Adding constants to the useraplication roles

--- SCV.Data/Configuration/ApplicationRoleConfiguration.cs
+++ SCV.Data/Configuration/ApplicationRoleConfiguration.cs
@@ -3,6 +3,7 @@
     using Microsoft.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore.Metadata.Builders;
     using SCV.Data.Models;
+    using SCV.GlCommon;
 
     public class ApplicationRoleConfiguration : IEntityTypeConfiguration<ApplicationRole>
     {
@@ -12,26 +13,26 @@
                 new ApplicationRole
                 {
                     Id = Guid.Parse("8add11c7-0c60-4776-9ad2-b598fa0f05ae"),
-                    Name = "Admin",
-                    NormalizedName = "ADMIN"
+                    Name = RoleConstants.Admin,
+                    NormalizedName = RoleConstants.Admin.ToUpperInvariant()
                 },
                 new ApplicationRole
                 {
                     Id = Guid.Parse("8d28163a-03ae-4e27-bc00-31d529cd6b52"),
-                    Name = "Manager",
-                    NormalizedName = "MANAGER"
+                    Name = RoleConstants.Manager,
+                    NormalizedName = RoleConstants.Manager.ToUpperInvariant()
                 },
                 new ApplicationRole
                 {
                     Id = Guid.Parse("e850a970-b0cd-40a1-ad09-4903d92d4d62"),
-                    Name = "Trainer",
-                    NormalizedName = "TRAINER"
+                    Name = RoleConstants.Trainer,
+                    NormalizedName = RoleConstants.Trainer.ToUpperInvariant()
                 },
                 new ApplicationRole
                 {
                     Id = Guid.Parse("761adbdb-1d7f-4dbb-8ec1-4d62fd0acde9"),
-                    Name = "User",
-                    NormalizedName = "USER"
+                    Name = RoleConstants.User,
+                    NormalizedName = RoleConstants.User.ToUpperInvariant()
                 });
         }
     }
---
+++ SCV.GlCommon/RoleConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace SCV.GlCommon
+{
+    public static class RoleConstants
+    {
+        public const string Admin = "Admin";
+
+        public const string Manager = "Manager";
+
+        public const string Trainer = "Trainer";
+
+        public const string User = "User";
+    }
+}
