1f3b6bf5c2298a3a7b0747ed90533859b10669f9
Rado <sitcom93@abv.bg>
2025-08-02 02:04:41 +0300

    Uploaing e Email Sendet via Gmail.

---
+++ SCV.Web.ViewModels/ContactVM/ContactFormViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace SCV.Web.ViewModels.ContactVM
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class ContactFormViewModel
+    {
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        public string? PhoneNumber { get; set; }
+
+        [Required]
+        public string Message { get; set; } = null!;
+    }
+}
--- SportCenterVictory/Controllers/ContactController.cs
+++ SportCenterVictory/Controllers/ContactController.cs
@@ -3,13 +3,52 @@
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
 
+    using SCV.Services.Core.Contracts;
+    using SCV.Web.ViewModels.ContactVM;
+
+    using static SCV.GlCommon.ApplicationConstants;
+
     public class ContactController : BaseController
     {
+        private readonly IEmailService emailService;
+
+        public ContactController(IEmailService emailService)
+        {
+            this.emailService = emailService;
+        }
+
         [HttpGet]
         [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(ContactFormViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            try
+            {
+                bool isSend = await emailService.SendContactEmailAsync(model);
+                string 
+
+                if (!isSend)
+                {
+                    TempData[ErrorMessageKey] = "Message was now sent. Try again later!";
+                    return RedirectToAction(nameof(Index));
+                }
+
+                TempData[SuccessMessageKey] = "Your message has been sent successfully!";
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                TempData[ErrorMessageKey] = "Something wen wrong. Please try again.";
+                return RedirectToAction(nameof(Index));
+            }
         }
     }
 }
